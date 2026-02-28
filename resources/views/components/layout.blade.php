a5ca8a45fbe4766537ded5285041792eb7222881
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-02-28 16:46:42 +0300

    google fonts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,36 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Pixel Positions</title>
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+    {{-- Google Fonts --}}\
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="bg-black text-white font-sans">
-  <div class="px-10">
-    <nav class="flex justify-between items-center py-4 border-b border-white/10">
-      <div>
-        <a href="/">
-          <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
-        </a>
-      </div>
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
 
-      <div class="space-x-6 font-bold">
-        <a href="#">Jobs</a>
-        <a href="#">Careers</a>
-        <a href="#">Salaries</a>
-        <a href="#">Companies</a>
-      </div>
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
 
-      <div>
-        <a href="">Post a job</a>
-      </div>
-    </nav>
+            <div>
+                <a href="">Post a job</a>
+            </div>
+        </nav>
 
-    <main class="mt-10 max-w-[986px] mx-auto">
-      {{ $slot }}
-    </main>
-  </div>
+        <main class="mt-10 max-w-[986px] mx-auto">
+            {{ $slot }}
+        </main>
+    </div>
 </body>
+
 </html>
