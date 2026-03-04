fe30f66161f8661480e910ad8f4677abd6e85a56
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-03-04 16:31:40 +0300

    table relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,24 +2,31 @@
 
 namespace App\Models;
 
+use App\Models\Tag;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
 
+    public function tag(string $name)
+    {
+        $tag = Tag::firstOrCreate(['name' => $name]);
+
+        $this->tags()->attach($tag);
+
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
     public function employer()
     {
         return $this->belongsTo(Employer::class);
     }  
 }
-
-it('can have tags', function () {
-    $job = Job :: factory()->create();
-
-    $job->tag('Frontend');
-
-    expect($job->tags)->toHaveCount(1);
-});
