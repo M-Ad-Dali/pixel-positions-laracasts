41257590fd5e5851be638b70ce80688744311527
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-03-04 16:27:52 +0300

    add field from databaces

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,19 +1,21 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo />
     </div>
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-500">Laracasts</a>
+        <a href="#" class="self-start text-sm text-gray-500">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-400">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-400">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
     <div>
         <div>
-            <x-tag size="small">Tag</x-tag>
-            <x-tag size="small">Tag</x-tag>
-            <x-tag size="small">Tag</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag :$tag />
+            @endforeach
         </div>
 
     </div>
