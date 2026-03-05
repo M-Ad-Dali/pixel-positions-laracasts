f13d0a2dcad5ffb09068190db998d5ea845c03e9
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-03-05 03:17:16 +0300

    click in job from to url

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -2,8 +2,14 @@
 
 <x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">{{ $job->employer->name }}</div>
+    
     <div class="py-8">
-        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-400">{{ $job->title }}</h3>
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-400">
+            <a href="{{ $job->url }}" target="_blank">
+                {{ $job->title }}
+            </a>
+
+        </h3>
         <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
 
@@ -14,7 +20,7 @@
             @endforeach
         </div>
 
-        <x-employer-logo/>
+        <x-employer-logo />
     </div>
 
 </x-panel>
